22671f65eca28c3ba16737a73f75fa98ce787578
Rohit <dv6510@gmail.com>
2026-01-16 02:47:28 +0530

    init: endpoints: register index router in v1 api_router

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,11 @@
+"""
+API router for v1 endpoints.
+"""
+
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import search
+from app.api.v1.endpoints import index, search
 
 api_router = APIRouter()
 api_router.include_router(search.router, prefix="/search", tags=["search"])
+api_router.include_router(index.router, prefix="/index", tags=["index"])
