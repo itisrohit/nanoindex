a40a35a137882d2ff1af85919eeadb3166c7e879
Rohit <dv6510@gmail.com>
2026-01-16 02:39:58 +0530

    init: internal package exposing mathematical primitives

---
+++ app/internal/__init__.py
@@ -0,0 +1,7 @@
+"""
+Core mathematical primitives for distance computation.
+"""
+
+from .distance import cosine_similarity, l2_distance, normalize_vectors
+
+__all__ = ["cosine_similarity", "l2_distance", "normalize_vectors"]
